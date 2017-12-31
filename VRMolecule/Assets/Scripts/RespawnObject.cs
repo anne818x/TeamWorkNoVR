472fc77d9a786d78569fe7121b89d19d6cc4949b
Anne <annemarieke-m@hotmail.com>
2017-12-31 15:56:47 +0100

    all

---
+++ VRMolecule/Assets/Scripts/RespawnObject.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnObject : MonoBehaviour {
+
+    public GameObject grabbableobj;
+
+    // Use this for initialization
+    void Start () {
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        positionCheck();
+    }
+
+    void positionCheck()
+    {
+        Vector3 currentpos = grabbableobj.GetComponent<Rigidbody>().transform.position;
+        if (float.Parse(currentpos.ToString()) > float.Parse(getSpawnPoint().ToString()) + 10 || float.Parse(currentpos.ToString()) < float.Parse(getSpawnPoint().ToString()) - 10)
+        {
+            grabbableobj.GetComponent<Rigidbody>().transform.position = getSpawnPoint();
+        }
+        else
+        {
+
+        }
+
+    }
+
+    Vector3 getSpawnPoint()
+    {
+        Vector3 initialposition = grabbableobj.GetComponent<Rigidbody>().transform.position;
+        return initialposition;
+    }
+
+}
---
+++ VRMolecule/Assets/Scripts/move.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour
+{
+    //Variables
+    public float speed = 6.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+
+    public float mouseSensitivity = 100.0f;
+    public float clampAngle = 80.0f;
+
+    private float rotY = 0.0f; // rotation around the up/y axis
+    private float rotX = 0.0f; // rotation around the right/x axis
+
+
+    void Start()
+    {
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotY = rot.y;
+        rotX = rot.x;
+    }
+
+    void Update()
+    {
+        // keyboard
+        CharacterController controller = GetComponent<CharacterController>();
+        // is the controller on the ground?
+        if (controller.isGrounded)
+        {
+            //Feed moveDirection with input.
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            //Multiply it by speed.
+            moveDirection *= speed;
+        }
+        //Applying gravity to the controller
+        moveDirection.y -= gravity * Time.deltaTime;
+        //Making the character move
+        controller.Move(moveDirection * Time.deltaTime);
+
+ 
+        // mouse
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = -Input.GetAxis("Mouse Y");
+
+        rotY += mouseX * mouseSensitivity * Time.deltaTime;
+        rotX += mouseY * mouseSensitivity * Time.deltaTime;
+
+        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+
+        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+        transform.rotation = localRotation;
+    }
+
+
+}
